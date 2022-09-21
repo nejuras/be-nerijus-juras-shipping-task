92b00f2d42e86bfad02931ee91af3f19d9f4f2cf
nerijus juras <nerusha@gmail.com>
2022-09-21 18:01:02 +0300

    command class rename

--- src/Command/RegisterShippingCommand.php
+++ src/Command/RegisterShippingCommand.php
@@ -10,7 +10,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class CreateCommand extends Command
+class RegisterShippingCommand extends Command
 {
     protected static $defaultName = 'app:register-shipment';
 
